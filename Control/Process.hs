a7ef3cc1241260e2225702f621d02633231a2dd1
Anton Rudnev <miborishante@gmail.com>
2009-02-26 20:31:13 +0300

    Import-export fix

--- Control/Process.hs
+++ Control/Process.hs
@@ -2,20 +2,15 @@ module Control.Process(
     module Control.Process.Action,
     module Control.Process.Process,
 
-    module Control.Monad,
-    module Control.Monad.Trans,
     module Control.Monad.State,
 
     module Control.Concurrent,
     module GHC.Conc) where
 
-
 import Control.Process.Process
 import Control.Process.Action
 
-import Control.Monad
-import Control.Monad.Trans
-import Control.Monad.State(MonadState(..))
+import Control.Monad.State(MonadState(..), MonadIO(..))
 
-import Control.Concurrent
+import Control.Concurrent(Chan(..), ThreadId)
 import GHC.Conc(ThreadStatus(..), BlockReason(..))
