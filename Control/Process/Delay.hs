7367b5f0649a7bedf13ce4584502e8bbfe868eb4
Dmitry Antonyuk <lomeo.nuke@gmail.com>
2009-03-13 11:51:19 +0300

    Used replicateM_

--- Control/Process/Delay.hs
+++ Control/Process/Delay.hs
@@ -1,16 +1,15 @@
 module Control.Process.Delay(threadDelayInteger, readChanDelay, readChanNow) where
 
 import Control.Concurrent
+import Control.Monad (replicateM_)
 import Foreign.Storable
 
 divt :: Integer -> (Integer, Int)
 divt long = (ii, fromInteger i) where (ii, i) = divMod long $ toInteger (maxBound :: Int) 
 
 threadDelayInteger :: Integer -> IO ()
-threadDelayInteger n = repeatThreadDelay ii >> threadDelay i where
+threadDelayInteger n = replicateM_ (fromInteger ii) (threadDelay maxBound) >> threadDelay i where
     (ii, i) = divt n
-    repeatThreadDelay 0 = return ()
-    repeatThreadDelay n = threadDelay maxBound >> repeatThreadDelay (n - 1)
 
 readChanDelay :: Chan ch -> Integer -> IO (Maybe ch)
 readChanDelay ch n = do
