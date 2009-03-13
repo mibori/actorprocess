ba0ebd54370af74868ab352eeca5da9bc23422f2
Dmitry Antonyuk <lomeo.nuke@gmail.com>
2009-03-13 11:50:18 +0300

    Used maxBound.

--- Control/Process/Delay.hs
+++ Control/Process/Delay.hs
@@ -3,20 +3,14 @@ module Control.Process.Delay(threadDelayInteger, readChanDelay, readChanNow) whe
 import Control.Concurrent
 import Foreign.Storable
 
-sizeOfInt :: Int
-sizeOfInt = sizeOf (0 :: Int)
-
-maxInt :: Int
-maxInt = 2 ^ (sizeOfInt * 8 - 1) - 1
-
 divt :: Integer -> (Integer, Int)
-divt long = (ii, fromInteger i) where (ii, i) = divMod long $ toInteger maxInt
+divt long = (ii, fromInteger i) where (ii, i) = divMod long $ toInteger (maxBound :: Int) 
 
 threadDelayInteger :: Integer -> IO ()
 threadDelayInteger n = repeatThreadDelay ii >> threadDelay i where
     (ii, i) = divt n
     repeatThreadDelay 0 = return ()
-    repeatThreadDelay n = threadDelay maxInt >> repeatThreadDelay (n - 1)
+    repeatThreadDelay n = threadDelay maxBound >> repeatThreadDelay (n - 1)
 
 readChanDelay :: Chan ch -> Integer -> IO (Maybe ch)
 readChanDelay ch n = do
